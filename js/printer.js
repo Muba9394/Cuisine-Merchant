57d9e135123d8a726ded5137b43197346a2deb7d
MubarakKhan <Muba9394@users.noreply.github.com>
2017-10-23 11:04:35 +0530

    change

--- js/printer.js
+++ js/printer.js
@@ -339,8 +339,8 @@ function conDevice(deviceId){
 	setTimeout(function(){
 		var uint8array = new TextEncoder('utf-8', { NONSTANDARD_allowLegacyEncoding: true }).encode(print_dtl);
      bt.write(uint8array.buffer, deviceId,1);	
-	 setTimeout(function(){window.location.reload();},1500);
-	},4000);
+	 setTimeout(function(){window.location.reload();},1000);
+	},2000);
 }
 
 function printorder(){
